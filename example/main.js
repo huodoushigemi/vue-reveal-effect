4350dc66411b9efa025daec9bbb5642545d03d8b
2629878269 <2629878269@qq.com>
2022-10-31 00:17:17 +0800

    style

--- example/main.js
+++ example/main.js
@@ -2,7 +2,7 @@ import { computed, createApp, watchEffect } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { isDark } from './dark'
-import RevealEffect, { setDefaultProps } from '../src/index'
+import RevealEffect, { setDefaultProps } from 'vue-reveal-effect'
 
 setDefaultProps({
   light: isDark
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'path'
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: './',
+  resolve: {
+    alias: {
+      'vue-reveal-effect': path.resolve(__dirname, './src/index')
+    }
+  },
   build: {
     outDir: 'docs'
   }
